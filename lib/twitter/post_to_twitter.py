fc99e163c362e1efe12bcf3adeeb894391e9089a
William Cook <wgeorgecook@gmail.com>
2017-11-01 13:32:50 -0700

    Adding time to logging

--- lib/twitter/post_to_twitter.py
+++ lib/twitter/post_to_twitter.py
@@ -3,6 +3,7 @@
 # All the tweepy stuff
 
 import tweepy
+from datetime import datetime
 from settings import CONSUMER_SETTINGS, ACCESS_SETTINGS, TWITTER_SETTINGS
 from lib.pollster.ratings_pull import get_data, CSV_URL
 from lib.twitter.read_from_twitter import retweet_status, read_status, twitter_ids
@@ -13,10 +14,11 @@ auth.set_access_token(ACCESS_SETTINGS.get('access_token'), ACCESS_SETTINGS.get('
 
 api = tweepy.API(auth)
 
+timestamp = datetime.now().strftime("%m/%d/%Y %H:%M")
 tweetID = read_status(TWITTER_SETTINGS.get('username'))
 
 def update_status(username):
-    print("ID's already tweeted:", twitter_ids)
+    print("Time is: ", timestamp) 
     ratings = get_data(CSV_URL)
     tweet = ".@realDonaldTrump's latest approval rating from Gallup is {0}% \n{1} ".format(ratings, retweet_status(username))
     api.update_status(tweet)
