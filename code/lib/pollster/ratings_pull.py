7940b55c8eb0cd9cb3603a9474e9ab231ae74256
William George Cook <wgeorgecook@gmail.com>
2018-01-04 08:22:25 -0600

    Fixing typo

--- code/lib/pollster/ratings_pull.py
+++ code/lib/pollster/ratings_pull.py
@@ -146,5 +146,4 @@ class DataRead(object):
                                             approval = int(approve_gallup.approve)
                                             disapproval = int(disapprove_gallup.disapprove)
                                             print(approval_is.format(approval), disapproval_is.format(disapproval))
-                                            return [approval, disapproval]
-                                        
+                                            return [approval, disapproval]                           
