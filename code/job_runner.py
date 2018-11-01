7d2ea0789c3d7adddeec4d8fbe8016adedebd99a
William George Cook <wgeorgecook@gmail.com>
2018-11-01 15:08:14 -0700

    Remove pollster import

--- code/job_runner.py
+++ code/job_runner.py
@@ -1,12 +1,13 @@
+#!/home/vagrant/python3_env/bin/python3
 # Runs the Python job
 
 
-from lib.pollster.ratings_pull import CSV_URL
+# from lib.pollster.ratings_pull import CSV_URL
 from settings import TWITTER_SETTINGS
-from lib.twitter import twitter_actions
+from lib.twitter.twitter_actions import Twinterface
 
 
-tweeting = twitter_actions.Twinterface()
+tweeting = Twinterface()
 tweeter = TWITTER_SETTINGS.get('username')
 
 
