d2072670731034c3d8d3c80e6d0c2e130e932a9f
William George Cook <wgeorgecook@gmail.com>
2019-04-06 15:55:33 -0700

    Add api file

---
+++ code/app.py
@@ -0,0 +1,84 @@
+import flask
+from flask_cors import CORS
+from flask import request, jsonify, Response, json
+from peewee import PostgresqlDatabase
+from lib.psql.models import Twitter_info
+from settings import DB_SETTINGS
+
+dbconnection = DB_SETTINGS.get('db')
+dbuser = DB_SETTINGS.get('user')
+dbpass = DB_SETTINGS.get('password')
+dbhost = DB_SETTINGS.get('host')
+
+psql_db = PostgresqlDatabase(
+    dbconnection,
+    user=dbuser,
+    password=dbpass,
+    host=dbhost,
+    )
+
+
+app = flask.Flask(__name__)
+CORS(app)
+app.config["DEBUG"] = True
+
+def custom_response(res):
+  """
+  Custom Response Function
+  """
+  return Response(
+    mimetype="application/json",
+    response=json.dumps(res.__dict__),
+    status=200
+  )
+
+@app.route("/", methods=['GET'])
+def home():
+    return """ <h1>This is the root of the Trumpratings API endpoint.</h1><h3>Please read the documentation to access data.</3>
+    """
+
+@app.route('/api/v1/resources/approvals', methods=['GET'])
+def approvals():
+    if 'id' in request.args:
+        tweet_id = int(request.args['id'])
+    else:
+        return "<h1>Please specify a tweet id</h1>"
+    psql_db.connect()
+    approval_ratings = Twitter_info.get(Twitter_info.tweet_id == tweet_id)
+    psql_db.close()
+    return jsonify(approval_ratings.approval_num)
+
+@app.route('/api/v1/resources/disapprovals', methods=['GET'])
+def disapprovals():
+    if 'id' in request.args:
+        tweet_id = int(request.args['id'])
+    else:
+        return "<h1>Please specify a tweet id</h1>"
+    psql_db.connect()
+    disapproval_ratings = Twitter_info.get(Twitter_info.tweet_id == tweet_id)
+    psql_db.close()
+    return jsonify(disapproval_ratings.disapproval_num)
+
+@app.route('/api/v1/resources/all', methods=['GET'])
+def all_ratings():
+    results = []
+    psql_db.connect()
+    all_ratings = Twitter_info.select().order_by(Twitter_info.tweet_id.desc())
+    for result in all_ratings:
+        results.append(
+            {'tweet_id': result.tweet_id, 'tweet_url': result.twitter_url, 'approval': result.approval_num, 'disapproval': result.disapproval_num, 'date_posted': result.date_posted, 'tweet_text': result.tweet_text})
+    psql_db.close()
+    return jsonify(results)
+
+@app.route('/api/v1/resources/latest', methods=['GET'])
+def new_ratings():
+    results = []
+    psql_db.connect()
+    for result in Twitter_info.select().order_by(Twitter_info.tweet_id.desc()):
+        results.append(
+            {'tweet_id': str(result.tweet_id), 'tweet_url': result.twitter_url, 'approval': result.approval_num, 'disapproval': result.disapproval_num, 'date_posted': result.date_posted, 'tweet_text': result.tweet_text})
+    psql_db.close()
+    return jsonify(results[:10])
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0") # vagrant
